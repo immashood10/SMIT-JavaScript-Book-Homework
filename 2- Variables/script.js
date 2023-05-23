ec4d584ae524d1caf41e41a41fcdf1062e90af66
Muhammad Mashood Khan <115594722+immashood10@users.noreply.github.com>
2023-05-23 17:28:33 +0500

    Add files via upload

---
+++ 2- Variables/script.js
@@ -0,0 +1,119 @@
+        //1. Declare a variable
+        var username = "Shafiq";
+
+        //2. Declare a variable
+        var myName = "Muhammad Mashood Khan";
+
+        // 3. Write a script to
+// a. declare a JS variable, titled message
+// b. assign “Hello World” to the variable message
+// c. display the message in alert box
+
+       var message = "Hello World";
+       alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables
+// and show the data in alert boxes. Example:
+
+    var name = "Ali";
+    var age = "17 Years";
+    var institute = "SMIT";
+    var course = "Web & Mobile Appliation Development";
+
+    alert(name);
+    alert(age);
+    alert(institute);
+    alert(course);
+
+//    5. Write a script to display the following alert using one JS variable:
+
+alert("Pizza" + "\n" + "Pizz" + "\n" + "Piz" + "\n" + "Pi" + "\n" + "P");
+
+// 6. Fortune Teller
+var post = "Software Developer";
+var country = "USA";
+var married = "pata nahi";
+var kids = 2;
+
+alert("Your will be a " + post + " in " + country + " and married to " + married + " with " + kids + " kids.");
+
+// 7. Declare a variable called email & assign to it a string
+
+var email = "immashood10@gmail.com";
+alert("My email address is " + email);
+
+// 8. Declare a variable called book & give it the value “A smarter way to learn JavaScript”.
+var book = "'A Smarter way to Learn JavaScript.'";
+alert("I am trying to learn from this Book " + book);
+
+// 9.Use the variables, declared in exercise 4 & show the details giving meaning to data.
+
+var name = "My  name is Muhammad Mashood Khan ";
+var age = "I am 17 years Old ";
+var university = "I studies in SSUET ";
+var course = "I took Mobile & Cloud Computing";
+
+alert(name + age + university + course);
+
+// 10. Write a script to display this in browser through JS 
+
+document.write("Yay! I can write HTML content thorugh JavaScript.");        //1. Declare a variable
+        var username = "Shafiq";
+
+        //2. Declare a variable
+        var myName = "Muhammad Mashood Khan";
+
+        // 3. Write a script to
+// a. declare a JS variable, titled message
+// b. assign “Hello World” to the variable message
+// c. display the message in alert box
+
+       var message = "Hello World";
+       alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables
+// and show the data in alert boxes. Example:
+
+    var name = "Ali";
+    var age = "17 Years";
+    var institute = "SMIT";
+    var course = "Web & Mobile Appliation Development";
+
+    alert(name);
+    alert(age);
+    alert(institute);
+    alert(course);
+
+//    5. Write a script to display the following alert using one JS variable:
+
+alert("Pizza" + "\n" + "Pizz" + "\n" + "Piz" + "\n" + "Pi" + "\n" + "P");
+
+// 6. Fortune Teller
+var post = "Software Developer";
+var country = "USA";
+var married = "pata nahi";
+var kids = 2;
+
+alert("You will be a " + post + " in " + country + " and married to " + married + " with " + kids + " kids.");
+
+// 7. Declare a variable called email & assign to it a string
+
+var email = "immashood10@gmail.com";
+alert("My email address is " + email);
+
+// 8. Declare a variable called book & give it the value “A smarter way to learn JavaScript”.
+var book = "'A Smarter way to Learn JavaScript.'";
+alert("I am trying to learn from this Book " + book);
+
+// 9.Use the variables, declared in exercise 4 & show the details giving meaning to data.
+
+var name = "My  name is Muhammad Mashood Khan ";
+var age = "I am 17 years Old ";
+var university = "I studies in SMIT";
+var course = "I took Web & Mobile Application Development";
+
+alert(name + age + university + course);
+
+// 10. Write a script to display this in browser through JS 
+
+document.write("Yay! I can write HTML content thorugh JavaScript.");
