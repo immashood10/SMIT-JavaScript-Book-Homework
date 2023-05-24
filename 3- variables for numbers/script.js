98867f379035391b0e8cd5d173301d0d07f08d9b
Muhammad Mashood Khan <115594722+immashood10@users.noreply.github.com>
2023-05-24 21:24:24 +0500

    Add files via upload

---
+++ 3- variables for numbers/script.js
@@ -0,0 +1,21 @@
+        // Declare a variabe that shows your age
+        var age = 21;
+        alert("My age is " + age + " years old");
+
+    //Declare a variable that shows how many time your user visitec our Website
+    var visit = 25;
+    alert("You have visited this site " + visit + " times");
+
+    // Deaclare a variable Birth year shows your birth year to DOM
+    var birthyear = 2001;
+    var type = "Number";
+    document.write("My birth year is " + birthyear + "<br />");
+    document.write("Type of this variable is " + type + "<br />");
+
+    //Online Shopping Order
+    var storeName = "XYZ Clothing Store";
+    var visitorsname = "John Doe";
+    var product = "T-shirt";
+    var quantity = 5;
+
+    document.write(visitorsname + " has ordered " + quantity + " "+ product + "'s" + " on " + storeName);
