815c824d55c28e200b1f7c3f9acc97961866808b
Muhammad Mashood Khan <immashood10@gmail.com>
2023-06-16 22:37:46 +0500

    for loop

---
+++ 18-20 ForLoops/app.js
@@ -0,0 +1,13 @@
+for (var i =0 ; i < 5; i++) {
+    document.write("Hellow World"+ "<br />");
+}
+
+for (var i = 1; i <= 10; i++) {
+    document.write(i + "<br>");
+}
+
+var tblNumber = prompt("Enter a Number");
+
+for (var i = 1;i <= 10; i++){
+    document.write(tblNumber + "x" + i + "=" + tblNumber * i + "<br>");
+}
