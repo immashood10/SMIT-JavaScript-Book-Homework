e20d802c721e9e82b14ba9a79656586f6722f2dc
Muhammad Mashood Khan <immashood10@gmail.com>
2023-06-18 19:46:29 +0500

    update

--- 18-20 ForLoops/app.js
+++ 18-20 ForLoops/app.js
@@ -1,13 +1,49 @@
-for (var i =0 ; i < 5; i++) {
-    document.write("Hellow World"+ "<br />");
+//1. Write a program display "Hello World" 5 times.
+// for (var i =0 ; i < 5; i++) {
+//     document.write("Hellow World"+ "<br />");
+// }
+
+//2. Write a program to print numeric counting from 1 to 10.
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(i + "<br>");
+// }
+
+//3. Write a program to print table taken user prompt.
+
+// var tblNumber = prompt("Enter a Number");
+
+// for (var i = 1;i <= 10; i++){
+//     document.write(tblNumber + "x" + i + "=" + tblNumber * i + "<br>");
+// }
+
+//4. Write a program to print table taken user prompt and also length.
+
+// var table = prompt("Enter a Table");
+// var num = prompt("Enter a Number");
+
+// for (i = 1; i <= num; i++) {
+//     document.write(table + "x" + i + "=" + table * i + "<br />");
+// }
+
+//4. you have an array write an array in new line with the help of loop.
+
+// a = ["Nokia","Samsung","Huawei","iPhone","Motorola"];
+
+// for(i = 0; i < a.length; i++) {
+//     document.write(a[i] + "<br>");
+// }
+
+//5. wirte an array using loop with index.
+
+fruits = ["Apple","Mango","Banana","Orange","PineApple"];
+
+for(i = 0; i < fruits.length; i++) {
+        document.write(fruits[i] + "<br>");
 }
 
-for (var i = 1; i <= 10; i++) {
-    document.write(i + "<br>");
+for(i = 0; i < fruits.length; i++) {
+    document.write("Element at Index " + i + " " + "is"  + " " + fruits[i] + "<br>");
+
 }
 
-var tblNumber = prompt("Enter a Number");
-
-for (var i = 1;i <= 10; i++){
-    document.write(tblNumber + "x" + i + "=" + tblNumber * i + "<br>");
-}
